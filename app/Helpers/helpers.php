4d1b8aa11120c8de11d33bca2425cdf4d9400c4e
Thiago Dias <tdias25@outlook.com>
2019-10-14 20:56:05 -0300

    helpers.php atualizado

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,6 +1,8 @@
 <?php
 
 
-function createDateByMonth($month = 1, $year = 2017) {
-	return "{$year}-{$month}-1";
+function createDateByMonth($month = 01, $year = 2017) {
+
+	$month = ltrim($month, '0');
+	return DateTime::createFromFormat('Y-m-d', "{$year}-$month-01")->format('Y-m-d');
 }
